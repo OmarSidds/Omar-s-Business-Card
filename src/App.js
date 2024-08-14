257a7deef1f4021f23c16c3d1736bba8b3148ee7
OmarSidds <101479068+OmarSidds@users.noreply.github.com>
2024-08-14 15:31:05 -0400

    80% done with the project. Need to finish paragraphs and footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import './App.css';
+import profileImg from './resumePicture.JPG'
+import linkedinIcon from './linkedinIcon.svg'
 
 function App() {
   return (
-    <h1>Omar</h1>
+    <div className='mainBox'>
+      <div className='mainContent'>
+        <div className='imgDiv'>
+          <img src={profileImg} className='profileImg'></img>
+        </div>
+        <div className='textContent'>
+          <h1>Omar Siddiqui</h1>
+          <h2>Frontend Developer</h2>
+          <div className='links'>
+            <a href='mailto:OSOmar120@gmail.com' className='linkEmail'>
+              <span class="material-symbols-outlined">mail</span>
+              Email</a>
+            <a href='https://www.linkedin.com/in/omar-siddiqui954/' className='linkLinkedin'>
+            <img src={linkedinIcon} className='linkedinIcon'></img>
+            Linkedin</a>
+          </div>
+          <div className='aboutAndInterestsDiv'>
+            <h3>About</h3>
+            <p>A frontend developer with a interest in learning all things technology.
+              Currently honing his skills in react and then processing to backend technologies 
+              and later cloud technologies.
+            </p>
+            <h4>Interests</h4>
+            <p>Playing, recording and creating music. Pickleball. Volleyball. Gym. Overwatch. 
+              Fortnite. Yugioh. Trying new resturants. 
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
 export default App;
+
+{/* <img src={profileImg} className='profileImg'></img> */}
